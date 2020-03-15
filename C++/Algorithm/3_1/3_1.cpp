bfecd512d4b14c8173d744e2c062ceb8f15e8936
Liu-Xue-Song <1169564849@qq.com>
2020-03-16 00:05:07 +0800

    1

---
+++ C++/Algorithm/3_1/3_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int search(int *a, int k, int end, int n)
+{
+    if (end - k < 2)
+        return a[k];
+    // else
+    //     while
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int *a = new int[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+}
--- Python/Algorithm/3_1.py
+++ Python/Algorithm/3_1.py
@@ -10,25 +10,36 @@ def odd(lst, k, end, n):
         #     lst[k], lst[end - 1] = lst[end - 1], lst[k]
         return lst[int((n - 1) / 2)]
     else:
-        temp = lst[k]
-        b = lst[:]
+        # temp = lst[k]
+        # b = lst[:]
+        # e = end - 1
+        # j = k
+        # for i in range(k + 1, end):
+        #     if lst[i] < temp:
+        #         b[j] = lst[i]
+        #         j += 1
+        #     else:
+        #         b[e] = lst[i]
+        #         e -= 1
+        # b[j] = temp
+        # lst = b.copy()
         e = end - 1
-        j = k
-        for i in range(k + 1, end):
-            if lst[i] < temp:
-                b[j] = lst[i]
-                j += 1
-            else:
-                b[e] = lst[i]
-                e -= 1
-        b[j] = temp
-        lst = b.copy()
-        if j == int((n - 1) / 2):
-            return b[j]
-        elif j > int((n - 1) / 2):
-            return odd(lst, k, j, n)
+        temp = lst[k]
+        s = k
+        while s < e:
+            while lst[e] > temp:
+                e = e - 1
+            lst[s] = lst[e]
+            while lst[s] < temp:
+                s += 1
+            lst[e] = lst[s]
+        lst[e] = temp
+        if e == int((n - 1) / 2):
+            return lst[e]
+        elif e > int((n - 1) / 2):
+            return odd(lst, k, e, n)
         else:
-            return odd(lst, j + 1, end, n)
+            return odd(lst, e + 1, end, n)
 
 
 r = (odd(num, 0, n, n))
